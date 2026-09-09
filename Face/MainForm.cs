2c20dcd19c2945e9b798b4e918297be230297daf
Dr. Mohamad Amir Dliwati <mohamad.amir.dliwati@gmail.com>
2026-09-09 09:50:30 +0300

    Add generated resource designer file for Properties.Resources

---
+++ Face/MainForm.cs
@@ -0,0 +1,225 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Diagnostics;
+
+namespace FaceRec_MM
+{
+    public partial class FrmPrincipal : Form
+    {
+        //التصريح عن المتحولات
+        bool Stop;
+        Image<Bgr, Byte> currentFrame;
+        Capture grabber;
+        HaarCascade face;
+        HaarCascade eye;
+        MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 0.5d, 0.5d);
+        Image<Gray, byte> result, TrainedFace = null;
+        Image<Gray, byte> gray = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels= new List<string>();
+        List<string> NamePersons = new List<string>();
+        int ContTrain, NumLabels, t;
+        string name, names = null;
+
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            Stop = false;
+            button1.Enabled = true;
+            button3.Enabled = false;
+            //Load haarcascades for face detection
+            face = new HaarCascade("haarcascade_frontalface_default.xml");
+           
+            try
+            {
+                //تحميل الوجوه السابقة
+                string Labelsinfo = File.ReadAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt");
+                string[] Labels = Labelsinfo.Split('%');
+                NumLabels = Convert.ToInt16(Labels[0]);
+                ContTrain = NumLabels;
+                string LoadFaces;
+
+                for (int tf = 1; tf < NumLabels+1; tf++)
+                {
+                    LoadFaces = "face" + tf + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/TrainedFaces/" + LoadFaces));
+                    labels.Add(Labels[tf]);
+                }
+            
+            }
+            catch(Exception e)
+            {
+              
+                MessageBox.Show("لا توجد قاعدة بيانات مسبقة", "جاري تحميل الوجوه", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           //تهيئة الكاميرا 
+            grabber = new Capture();
+            grabber.QueryFrame();
+            //تهيئة Frame
+            Application.Idle += new EventHandler(FrameGrabber);
+            button1.Enabled = false;
+            button3.Enabled = true;
+            Stop = false;
+        }
+
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                //uu] hg,[,i td hgw,vm
+                ContTrain = ContTrain + 1;
+
+                //التحويل إلى Gray
+                gray = grabber.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+                //اكتشاف الوجه
+                MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(face,1.2,10,
+                Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                new Size(20, 20));
+
+                //التدريب على الوجوه
+                foreach (MCvAvgComp f in facesDetected[0])
+                {
+                    TrainedFace = currentFrame.Copy(f.rect).Convert<Gray, byte>();
+                    break;
+                }
+
+                // ملائمة الحجوم
+                TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                trainingImages.Add(TrainedFace);
+                labels.Add(textBox1.Text);
+
+                //الوجوه المضافة
+                imageBox1.Image = TrainedFace;
+
+                //حفظ الوجوه
+                File.WriteAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", trainingImages.ToArray().Length.ToString() + "%");
+
+                //حفظ الأسماء
+                for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+                {
+                    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/TrainedFaces/face" + i + ".bmp");
+                    File.AppendAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", labels.ToArray()[i - 1] + "%");
+                }
+
+                MessageBox.Show(textBox1.Text + "´s face detected and added :)", "Training OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Enable the face detection first", "Training Fail", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+
+        void FrameGrabber(object sender, EventArgs e)
+        {
+            label3.Text = "0";
+            // التعرف على الوجوه
+            NamePersons.Add("");
+            if (Stop) return;
+
+            currentFrame = grabber.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+                    gray = currentFrame.Convert<Gray, Byte>();
+
+                    MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(
+                  face,
+                  1.2,
+                  10,
+                  Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                  new Size(20, 20));
+
+                    foreach (MCvAvgComp f in facesDetected[0])
+                    {
+                        t = t + 1;
+                        result = currentFrame.Copy(f.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                        currentFrame.Draw(f.rect, new Bgr(Color.Red), 2);
+
+
+                        if (trainingImages.ToArray().Length != 0)
+                        {
+                        MCvTermCriteria termCrit = new MCvTermCriteria(ContTrain, 0.001);
+
+                        //Eigen face recognizer
+                        EigenObjectRecognizer recognizer = new EigenObjectRecognizer(
+                           trainingImages.ToArray(),
+                           labels.ToArray(),
+                           1800,       
+                           ref termCrit);
+
+                        name = recognizer.Recognize(result);
+                        currentFrame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.LightGreen));
+
+                        }
+
+                            NamePersons[t-1] = name;
+                            NamePersons.Add("");
+
+                        label3.Text = facesDetected[0].Length.ToString();
+                       
+                     
+
+                    }
+                        t = 0;
+
+                       
+                    for (int nnn = 0; nnn < facesDetected[0].Length; nnn++)
+                    {
+                        names = names + NamePersons[nnn] + ", ";
+                    }
+
+                    imageBoxFrameGrabber.Image = currentFrame;
+                    label4.Text = names;
+                    names = "";
+                    NamePersons.Clear();
+
+                }
+
+        private void imageBoxFrameGrabber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button3.Enabled = false;
+            grabber.Dispose();
+            Stop= true;
+        }
+
+     
+
+    }
+}
